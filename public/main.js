546663b076fc49492be5c60b736e5cdf63eb069d
opariffazman <opariffazman@gmail.com>
2022-09-30 18:07:36 +0800

    debug window.onload

--- public/main.js
+++ public/main.js
@@ -2,66 +2,64 @@ const goToAddSlide = () => {
   window.location.assign("/add")
 }
 
-async function initializeSlides() {
-  let slides
-
-  const res = await fetch('https://slides.cyclic.app/api/listJson')
-
-  slides = await res.json()
-
-  let container = document.getElementById('main')
-  for (let index = 0; index < slides.length; index++) {
-    let slide = slides[index].Key
-    slide = slide.replace(".json", "")
-
-    let slideContent
-
-    const res = await fetch(`https://slides.cyclic.app/api/files?name=${slide}`)
-
-    slideContent = await res.json()
-
-    let slideId = slideContent['id']
-    let slideName = slideContent['name']
-    let slideDesc = slideContent['description']
-    let slidePrev = slideContent['preview']
-    let slidePrice = slideContent['price']
-    let slideUrl = slideContent['url']
-
-    console.log(slideId, slideName, slideDesc, slidePrev, slidePrice, slideUrl)
-    let button = isAdmin() ? "Edit" : "View"
-    let div = document.createElement("div")
-    div.classList.add("col");
-    div.innerHTML = `
-    <div class="card shadow-sm">
-    <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg"
-      role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false">
-      <title>Placeholder</title>
-      <rect width="100%" height="100%" fill="#55595c" />
-      <text x="50%" y="50%" fill="#eceeef" dy=".3em">${slidePrev}</text>
-    </svg>
-
-    <div class="card-body">
-      <p class="card-text"><b>${slideName}</b></br><i>${slideDesc}</i></p>
-      <div class="d-flex justify-content-between align-items-center">
-        <div class="btn-group">
-          <button type="button" class="btn btn-sm btn-outline-secondary" data-bs-toggle="modal" data-bs-target="#slideModal"
-          data-bs-name="${slideName}"
-          data-bs-desc="${slideDesc}"
-          data-bs-preview="${slidePrev}"
-          data-bs-price="${slidePrice}">${button}</button>
-        </div>
-        <small class="text-muted">${slidePrice}</small>
-      </div>
-    </div>
-    `
-    container.appendChild(div)
-  }
-}
-
-
-
 window.onload = () => {
   const localStorage = window.localStorage
+
+  async function initializeSlides() {
+    let slides
+
+    const res = await fetch('https://slides.cyclic.app/api/listJson')
+
+    slides = await res.json()
+
+    let container = document.getElementById('main')
+    for (let index = 0; index < slides.length; index++) {
+      let slide = slides[index].Key
+      slide = slide.replace(".json", "")
+
+      let slideContent
+
+      const res = await fetch(`https://slides.cyclic.app/api/files?name=${slide}`)
+
+      slideContent = await res.json()
+
+      let slideId = slideContent['id']
+      let slideName = slideContent['name']
+      let slideDesc = slideContent['description']
+      let slidePrev = slideContent['preview']
+      let slidePrice = slideContent['price']
+      let slideUrl = slideContent['url']
+
+      console.log(slideId, slideName, slideDesc, slidePrev, slidePrice, slideUrl)
+      let button = isAdmin() ? "Edit" : "View"
+      let div = document.createElement("div")
+      div.classList.add("col");
+      div.innerHTML = `
+      <div class="card shadow-sm">
+      <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg"
+        role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false">
+        <title>Placeholder</title>
+        <rect width="100%" height="100%" fill="#55595c" />
+        <text x="50%" y="50%" fill="#eceeef" dy=".3em">${slidePrev}</text>
+      </svg>
+
+      <div class="card-body">
+        <p class="card-text"><b>${slideName}</b></br><i>${slideDesc}</i></p>
+        <div class="d-flex justify-content-between align-items-center">
+          <div class="btn-group">
+            <button type="button" class="btn btn-sm btn-outline-secondary" data-bs-toggle="modal" data-bs-target="#slideModal"
+            data-bs-name="${slideName}"
+            data-bs-desc="${slideDesc}"
+            data-bs-preview="${slidePrev}"
+            data-bs-price="${slidePrice}">${button}</button>
+          </div>
+          <small class="text-muted">${slidePrice}</small>
+        </div>
+      </div>
+      `
+      container.appendChild(div)
+    }
+  }
 
   const parseJwt = (token) => {
     let base64Url = token.split('.')[1]
