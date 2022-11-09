62d8ee641d941cd639417a5134d999b60e21fbd9
opariffazman <opariffazman@gmail.com>
2022-11-09 22:27:40 +0800

    changed submit event listener to click

--- public/form-validation.js
+++ public/form-validation.js
@@ -7,7 +7,7 @@
 
   // Loop over them and prevent submission
   Array.from(forms).forEach(form => {
-    form.addEventListener('submit', event => {
+    form.addEventListener('click', event => {
       if (!form.checkValidity()) {
         event.preventDefault()
         event.stopPropagation()
